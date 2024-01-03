5d2bcc1db8a59b80ffe0adc5dcb3cdd75b1ca568
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-03 10:55:12 +0700

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FiThumbsUp } from 'react-icons/fi';
+
+export default function Card({ result }) {
+  return (
+    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          width={500}
+          height={300}
+          className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
+        ></Image>
+        <div className='p-2'>
+          <p className='line-clamp-2 text-md'>{result.overview}</p>
+          <h2 className='text-lg font-bold truncate'>
+            {result.title || result.name}
+          </h2>
+          <p className='flex items-center'>
+            {result.release_date || result.first_air_date}
+            <FiThumbsUp className='h-5 mr-1 ml-3'/>
+            {result.vote_count}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+}
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,10 @@
+import Card from './Card';
+
 export default function Results({ results }) {
   return (
-    <div>
+    <div className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4'>
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
-        </div>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
