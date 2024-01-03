f1824f738c73fff13612019f8369af94e0afe144
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-03 12:17:10 +0700

    fix the error.jsx file

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-export default function error({ error, reset }) {
+export default function Error({ error, reset }) {
   useEffect(() => {
     console.log(error);
   }, [error]);
