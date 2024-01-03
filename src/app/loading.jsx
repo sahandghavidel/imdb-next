5999db050c4d09f9be0fbd82b86ecfc1ffb614a4
Sahand Ghavidel <sahand.gha@gmail.com>
2024-01-03 10:14:02 +0700

    add loading effect using nextjs

---
+++ src/app/loading.jsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-16'>
+      <img className='h-52' src='spinner.svg' alt='loading...' />
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ export default async function Home({ searchParams }) {
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
   const data = await res.json();
   if (!res.ok) {
